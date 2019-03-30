c4264e39b65bade9c0cca1fb90c0e2408c130271
curtisjohnson <curtis.johnson@roguewave.com>
2019-03-29 19:33:21 -0700

    change

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -46,7 +46,7 @@ import org.springframework.samples.petclinic.model.Person;
 @Table(name = "owners")
 public class Owner extends Person {
     @Column(name = "address")
-    @NotEmpty
+//    @NotEmpty
     private String address;
 
     @Column(name = "city")
